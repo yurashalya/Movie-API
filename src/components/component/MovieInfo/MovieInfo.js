bacef72b2e2f4e86d42d36af362c0f4c2b7b0c56
Yura Shalya <shalyayura@gmail.com>
2019-01-31 00:24:22 +0200

    passing parameters to components

--- src/components/component/MovieInfo/MovieInfo.js
+++ src/components/component/MovieInfo/MovieInfo.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import  { IMAGE_URL, POSTER_SIZE, BACK_SIZE } from '../../../Configs/Configs';
+import  { IMAGE_URL, POSTERS_SIZE, BACK_SIZE } from '../../../Configs/Configs';
 import FontAwesome from 'react-fontawesome';
 import MovieWatch from '../MovieWatch/MovieWatch';
 
+import classes from './MovieInfo.module.css';
+
 
 const MovieInfo = (props) => (
-    <div>MovieInfo</div>
+    <div className={classes.Movie_info}
+    style={{
+      background: props.movie.backdrop_path ? `url('${IMAGE_URL}${BACK_SIZE}${props.movie.backdrop_path}')` : '#000'
+    }}
+  >
+    <div className={classes.Movie_info_content}>
+      <div className={classes.Movie_info_watch}>
+        <MovieWatch
+          image={props.movie.poster_path ? `${IMAGE_URL}${POSTERS_SIZE}${props.movie.poster_path}` : '../../../assets/images/noimg.jpg'}
+          clicked={false}
+        />
+      </div>
+      <div className={classes.Movie_info_text}>
+        <h1>{props.movie.title}</h1>
+        <h3>PLOT</h3>
+        <p>{props.movie.overview}</p>
+        <h3>IMDB RATING</h3>
+        <div className={classes.Rating}>
+          <meter min="0" max="100" optimum="100" low="40" high="70" value={props.movie.vote_average * 10}></meter>
+          <p className={classes.Score}>{props.movie.vote_average}</p>
+        </div>
+        {props.directors.length > 1 ? <h3>DIRECTORS</h3> : <h3>DIRECTOR</h3>}
+        {props.directors.map( (element, i) => {
+          return <p key={i} className={classes.Director}>{element.name}</p>
+        })}
+      </div>
+      <FontAwesome className={classes.fa_film} name="film" size="5x" />
+    </div>
+  </div>
 );
 
 
--- src/components/component/Nav/Nav.js
+++ src/components/component/Nav/Nav.js
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom';
 import classes from './Nav.module.css';
 
 const Nav = (props) => (
-    <div>Navigatiin</div>
-);
+    <div className={classes.Navigation}>
+      <div className={classes.Navigation_content}>
+        <Link to="/">
+          <p>Home</p>
+        </Link>
+        <p>/</p>
+        <p>{props.movie}</p>
+      </div>
+    </div>
+  )
 
 
 export default Nav;
